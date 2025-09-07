b4b8f2b142b3ad5fd10680f48792f93fe1c1fd96
Danieltorres191 <johan.montoya-t@uniminuto.edu.co>
2025-09-07 15:56:02 -0500

    Update pacienteService.java

--- src/main/java/com/uniminuto/clinica/service/pacienteService.java
+++ src/main/java/com/uniminuto/clinica/service/pacienteService.java
@@ -1,1 +1,10 @@
+package com.uniminuto.clinica.service;
+
+import com.uniminuto.clinica.entity.Paciente;
+import java.util.List;
+
+public interface PacienteService {
+    List<Paciente> listarPacientes();
+    Paciente buscarPorDocumento(String documento);
+}
 
