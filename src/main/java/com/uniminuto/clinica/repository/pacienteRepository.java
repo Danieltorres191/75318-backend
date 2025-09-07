2a0b9894433c0421a2adff74676e4d0a57142185
Danieltorres191 <johan.montoya-t@uniminuto.edu.co>
2025-09-07 15:55:42 -0500

    Update pacienteRepository.java

--- src/main/java/com/uniminuto/clinica/repository/pacienteRepository.java
+++ src/main/java/com/uniminuto/clinica/repository/pacienteRepository.java
@@ -1,1 +1,10 @@
+package com.uniminuto.clinica.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.uniminuto.clinica.entity.Paciente;
+import java.util.Optional;
+
+public interface PacienteRepository extends JpaRepository<Paciente, Long> {
+    Optional<Paciente> findByDocumento(String documento);
+}
 
