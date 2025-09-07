2e8654292cc183ec816813e098e4dc1315791b38
Danieltorres191 <johan.montoya-t@uniminuto.edu.co>
2025-09-07 15:56:30 -0500

    Update pacienteServiceImpl.java

--- src/main/java/com/uniminuto/clinica/service/impl/pacienteServiceImpl.java
+++ src/main/java/com/uniminuto/clinica/service/impl/pacienteServiceImpl.java
@@ -1,1 +1,28 @@
+package com.uniminuto.clinica.service.impl;
+
+import com.uniminuto.clinica.entity.Paciente;
+import com.uniminuto.clinica.repository.PacienteRepository;
+import com.uniminuto.clinica.service.PacienteService;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class PacienteServiceImpl implements PacienteService {
+
+    private final PacienteRepository pacienteRepository;
+
+    public PacienteServiceImpl(PacienteRepository pacienteRepository) {
+        this.pacienteRepository = pacienteRepository;
+    }
+
+    @Override
+    public List<Paciente> listarPacientes() {
+        return pacienteRepository.findAll();
+    }
+
+    @Override
+    public Paciente buscarPorDocumento(String documento) {
+        return pacienteRepository.findByDocumento(documento).orElse(null);
+    }
+}
 
